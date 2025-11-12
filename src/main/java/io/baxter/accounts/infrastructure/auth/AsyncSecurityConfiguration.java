22574ba33b184fd4384126e480e570cc789ed4ec
robert baxter <baxterrp@gmail.com>
2025-11-12 07:40:53 -0500

    ignore auth config

--- src/main/java/io/baxter/accounts/infrastructure/auth/AsyncSecurityConfiguration.java
+++ src/main/java/io/baxter/accounts/infrastructure/auth/AsyncSecurityConfiguration.java
@@ -1,6 +1,6 @@
 package io.baxter.accounts.infrastructure.auth;
 
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +14,7 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 import javax.crypto.spec.SecretKeySpec;
 import java.util.Base64;
 
+@Generated
 @Configuration
 @EnableWebFluxSecurity
 @RequiredArgsConstructor
