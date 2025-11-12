3b4346dad2cdb3c01053c7fa808bcdaa6e57f7d4
robert baxter <baxterrp@gmail.com>
2025-11-12 07:35:01 -0500

    finished account controller tests

--- src/test/java/io/baxter/accounts/tests/api/controllers/AccountControllerTest.java
+++ src/test/java/io/baxter/accounts/tests/api/controllers/AccountControllerTest.java
@@ -2,6 +2,7 @@ package io.baxter.accounts.tests.api.controllers;
 
 import io.baxter.accounts.api.controllers.AccountController;
 import io.baxter.accounts.api.models.*;
+import io.baxter.accounts.api.models.register.*;
 import io.baxter.accounts.api.services.AccountService;
 import org.junit.jupiter.api.*;
 import org.mockito.*;
@@ -11,8 +12,6 @@ import reactor.test.StepVerifier;
 
 import java.util.UUID;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 @ControllerTest(controllers = AccountController.class)
 public class AccountControllerTest {
 
@@ -21,32 +20,87 @@ public class AccountControllerTest {
 
     private AccountController accountController;
 
+    final private UUID userId = UUID.fromString("fd9530d7-17dd-49fa-b187-b3d309cbbd9e");
+    final private String email = "test@test.com";
+    final private Integer id = 1;
+
     @BeforeEach
     void setup(){
         accountController = new AccountController(mockAccountService);
     }
 
     @Test
+    @DisplayName("register should return a valid account model when a valid registration is given")
+    void registerShouldReturnOkWithAccountModel(){
+        // Arrange
+        final PhoneModel expectedPhone = getPhoneModel();
+        final AddressModel expectedAddress = getAddressModel();
+        final String password = "Pass123##";
+        final RegistrationRequest request = new RegistrationRequest(userId, email, password, expectedPhone, expectedAddress);
+        final RegistrationResponse expectedResponse = new RegistrationResponse(id, userId, email);
+
+        Mockito.when(mockAccountService.register(request)).thenReturn(Mono.just(expectedResponse));
+
+        // Act
+        var response = accountController.register(request);
+
+        // Assert
+        StepVerifier.create(response)
+                .expectNextMatches(actual ->
+                        actual.getStatusCode().equals(HttpStatus.CREATED))
+                .verifyComplete();
+
+        Mockito.verify(mockAccountService).register(request);
+        Mockito.verifyNoMoreInteractions(mockAccountService);
+    }
+
+    @Test
+    @DisplayName("update should return a valid account model when a valid account is given")
+    void updateShouldReturnOkWithAccountModel(){
+        // Arrange
+        final String email = "test@test.com";
+        final PhoneModel expectedPhone = getPhoneModel();
+        final AddressModel expectedAddress = getAddressModel();
+        final AccountModel expectedAccountModel = new AccountModel(
+                id,
+                userId,
+                email,
+                expectedPhone,
+                expectedAddress);
+
+        final UpdateAccountRequest request = new UpdateAccountRequest(expectedPhone, expectedAddress);
+
+        Mockito.when(mockAccountService.updateAccount(request, id)).thenReturn(Mono.just(expectedAccountModel));
+
+        // Act
+        var response = accountController.update(id, request);
+
+        // Assert
+        StepVerifier.create(response)
+                .expectNextMatches(accountModel -> {
+                    var body = accountModel.getBody();
+                    var status = accountModel.getStatusCode();
+
+                    return status.equals(HttpStatus.OK) &&
+                            body != null &&
+                            comparePhones(expectedPhone, body.getPhone()) &&
+                            compareAddress(expectedAddress, body.getAddress());
+                })
+                .verifyComplete();
+
+        Mockito.verify(mockAccountService).updateAccount(request, id);
+        Mockito.verifyNoMoreInteractions(mockAccountService);
+    }
+
+    @Test
     @DisplayName("getAccountById should return a valid account model when a valid id is given")
     void getAccountByIdShouldReturnOkWithAccountModel(){
         // Arrange
-        final int id = 1;
-        final String email = "test@test.com";
-        final PhoneModel expectedPhone = new PhoneModel(
-                id,
-                "1234567890",
-                "1");
-        final AddressModel expectedAddress = new AddressModel(
-                id,
-                "123 fk st.",
-                "ypsilanti",
-                "mi",
-                "48198",
-                "USA");
-
+        final PhoneModel expectedPhone = getPhoneModel();
+        final AddressModel expectedAddress = getAddressModel();
         final AccountModel expectedAccountModel = new AccountModel(
                 id,
-                UUID.randomUUID(),
+                userId,
                 email,
                 expectedPhone,
                 expectedAddress);
@@ -71,6 +125,23 @@ public class AccountControllerTest {
 
         Mockito.verify(mockAccountService).getAccountById(id);
         Mockito.verifyNoMoreInteractions(mockAccountService);
+    }
+
+    private AddressModel getAddressModel(){
+        return new AddressModel(
+                id,
+                "123 fk st.",
+                "ypsilanti",
+                "mi",
+                "48198",
+                "USA");
+    }
+
+    private PhoneModel getPhoneModel(){
+        return new PhoneModel(
+                id,
+                "1234567890",
+                id.toString());
     }
 
     private static boolean compareAddress(AddressModel expectedAddress, AddressModel actualAddress){
