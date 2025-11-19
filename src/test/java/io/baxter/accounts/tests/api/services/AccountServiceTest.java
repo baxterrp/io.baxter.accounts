69bc24b4c261f0d1f720b42f98f5b8667fa9b5d1
robert baxter <baxterrp@gmail.com>
2025-11-19 06:02:51 -0500

    finish account service tests

--- src/test/java/io/baxter/accounts/tests/api/services/AccountServiceTest.java
+++ src/test/java/io/baxter/accounts/tests/api/services/AccountServiceTest.java
@@ -1,11 +1,12 @@
 package io.baxter.accounts.tests.api.services;
 
 import io.baxter.accounts.api.models.*;
+import io.baxter.accounts.api.models.register.RegistrationRequest;
 import io.baxter.accounts.api.services.AccountServiceImpl;
 import io.baxter.accounts.data.models.*;
 import io.baxter.accounts.data.repository.*;
 import io.baxter.accounts.infrastructure.behavior.exceptions.ResourceNotFoundException;
-import io.baxter.accounts.infrastructure.messaging.AccountEventPublisher;
+import io.baxter.accounts.infrastructure.messaging.*;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.*;
@@ -15,6 +16,9 @@ import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import java.util.UUID;
+
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.doNothing;
 
 @ExtendWith(MockitoExtension.class)
 @ExtendWith(OutputCaptureExtension.class)
@@ -41,7 +45,8 @@ public class AccountServiceTest {
         StepVerifier.create(response)
                 .expectErrorMatches(error ->
                         error instanceof ResourceNotFoundException &&
-                        error.getMessage().equals("No account found with id 1"));
+                        error.getMessage().equals("No account found with id 1"))
+                .verify();
 
         Mockito.verify(mockAccountRepository).findById(accountId);
     }
@@ -56,9 +61,9 @@ public class AccountServiceTest {
                 accountId, userId.toString(), "test", "user", "test@test.com", accountId, accountId);
 
         Mockito.when(mockAccountRepository.findById(accountId)).thenReturn(Mono.just(expectedAccountDataModel));
-        Mockito.when(mockPhoneNumberRepository.save(Mockito.any())).thenReturn(Mono.just(expectedPhoneDataModel));
-        Mockito.when(mockAddressRepository.save(Mockito.any())).thenReturn(Mono.just(expectedAddressDataModel));
-        Mockito.when(mockAccountRepository.save(Mockito.any())).thenReturn(Mono.just(expectedAccountDataModel));
+        Mockito.when(mockPhoneNumberRepository.save(any())).thenReturn(Mono.just(expectedPhoneDataModel));
+        Mockito.when(mockAddressRepository.save(any())).thenReturn(Mono.just(expectedAddressDataModel));
+        Mockito.when(mockAccountRepository.save(any())).thenReturn(Mono.just(expectedAccountDataModel));
 
         // update account method ends with a call get getAccountByUserId
         Mockito.when(mockAccountRepository.findByUserId(userId)).thenReturn(Mono.just(expectedAccountDataModel));
@@ -79,6 +84,74 @@ public class AccountServiceTest {
                 .verifyComplete();
 
         Mockito.verify(mockAccountRepository).findById(accountId);
+    }
+
+    @Test
+    void registerWhenEmailOrUserIdExistsReturnsResourceExistsException(){
+        // Arrange
+        var registrationRequest = new RegistrationRequest(
+                userId, "test", "user", "test@test.com", "123pass##", getPhoneModel(), getAddressModel());
+
+        Mockito.when(mockAccountRepository.existsByEmailOrUserId(eq(registrationRequest.getEmail()), eq(userId.toString())))
+                .thenReturn(Mono.just(true));
+
+        // Act
+        var response = accountService.register(registrationRequest);
+
+        // Assert
+        StepVerifier.create(response)
+                .expectErrorMatches(error -> error.getMessage().equals("account already exists with value test@test.com"))
+                .verify();
+    }
+
+    @Test
+    void registerWhenEmailAndUserIdNotExistReturnsRegisteredUserAndPublishesAccountRegisteredEvent(){
+        // Arrange
+        var expectedAccount = getAccountModel();
+        var expectedPhoneDataModel = new PhoneNumberDataModel(accountId, "1234567890", "1");
+        var expectedAddressDataModel = new AddressDataModel(accountId, "123 fk st.", "ypsilanti", "MI", "48198", "USA");
+        var expectedAccountDataModel = new AccountDataModel(
+                accountId, userId.toString(), "test", "user", "test@test.com", accountId, accountId);
+        var registrationRequest = new RegistrationRequest(
+                userId, "test", "user", "test@test.com", "123pass##", getPhoneModel(), getAddressModel());
+
+        Mockito.when(mockAccountRepository.existsByEmailOrUserId(eq("test@test.com"), eq(userId.toString()))).thenReturn(Mono.just(false));
+        Mockito.when(mockPhoneNumberRepository.save(any(PhoneNumberDataModel.class))).thenReturn(Mono.just(expectedPhoneDataModel));
+        Mockito.when(mockAddressRepository.save(any(AddressDataModel.class))).thenReturn(Mono.just(expectedAddressDataModel));
+        Mockito.when(mockAccountRepository.save(any(AccountDataModel.class))).thenReturn(Mono.just(expectedAccountDataModel));
+
+        doNothing()
+                .when(mockEventPublisher)
+                .publishAccountRegistered(argThat(registeredEvent -> registeredEvent.getUserId().equals(userId)));
+
+        // Act
+        var response = accountService.register(registrationRequest);
+
+        // Assert
+        StepVerifier.create(response)
+                .expectNextMatches(account ->
+                        account.getId().equals(expectedAccount.getId()) &&
+                        account.getUserId().equals(expectedAccount.getUserId()) &&
+                        account.getEmail().equals(expectedAccount.getEmail()))
+                .verifyComplete();
+    }
+
+    @Test
+    void getAccountWhenNoAccountFoundReturnsResouceNotFoundException(){
+        // Arrange
+        Mockito.when(mockAccountRepository.findByUserId(userId)).thenReturn(Mono.empty());
+
+        // Act
+        var response = accountService.getAccountByUserId(userId);
+
+        // Assert
+        StepVerifier.create(response)
+                .expectErrorMatches(error ->
+                        error instanceof ResourceNotFoundException &&
+                                error.getMessage().equals("No account found with id c284e1b4-214a-4e36-886b-63d6d4279e6a"))
+                .verify();
+
+        Mockito.verify(mockAccountRepository).findByUserId(userId);
     }
 
     @Test
